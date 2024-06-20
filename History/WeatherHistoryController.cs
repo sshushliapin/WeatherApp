2295821af7a4a79efee5d3b7cdfafe9c4e214f49
Serhii Shushliapin <dev@sshushliapin.io>
2024-06-20 23:40:59 +0300

    Introduce WeatherHistory endpoint

---
+++ History/WeatherHistoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace WeatherApp.History;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class WeatherHistoryController : ControllerBase
+{
+    [HttpGet(Name = "GetWeatherHistory")]
+    public IEnumerable<WeatherHistoryRecord> Get()
+    {
+        return [];
+    }
+}
---
+++ History/WeatherHistoryRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApp.History;
+
+public class WeatherHistoryRecord
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+}
