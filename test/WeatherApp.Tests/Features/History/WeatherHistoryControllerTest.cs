2c6d4776e4da30245a9779d7a8f0f1265262c94f
Serhii Shushliapin <dev@sshushliapin.io>
2024-06-21 15:10:05 +0300

    Add controller tests

---
+++ test/WeatherApp.Tests/Features/History/WeatherHistoryControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture.Xunit2;
+using CSharpFunctionalExtensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using WeatherApp.Features.History;
+
+namespace WeatherApp.Tests.Features.History;
+
+public class WeatherHistoryControllerTest
+{
+    [Theory, AutoNSubstituteData]
+    public async Task PostWithInvalidRequestReturnsBadRequest(
+        [Frozen] IMediator mediatr,
+        [NoAutoProperties] WeatherHistoryController sut,
+        CreateHistoryRecord.CreateHistoryRecordRequest request,
+        string error)
+    {
+        mediatr.Send(request).Returns(Result.Failure(error));
+        var result = await sut.Post(request);
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    [Theory, AutoNSubstituteData]
+    public async Task PostWithValidRequestReturnsCreated(
+        [NoAutoProperties] WeatherHistoryController sut,
+        CreateHistoryRecord.CreateHistoryRecordRequest request)
+    {
+        var result = await sut.Post(request);
+        Assert.IsType<CreatedResult>(result);
+    }
+}
