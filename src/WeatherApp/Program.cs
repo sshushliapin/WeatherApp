b1c22eee37fc258c8b5e98666073853be35a2c6c
Serhii Shushliapin <dev@sshushliapin.io>
2024-06-24 11:42:56 +0300

    Configure Swagger auth

--- src/WeatherApp/Program.cs
+++ src/WeatherApp/Program.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 using WeatherApp.Database;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,42 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHealthChecks();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    // Configure OAuth2
+    c.AddSecurityDefinition("OAuth2", new OpenApiSecurityScheme
+    {
+        Name = "OAuth2",
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri(builder.Configuration["SwaggerAzureAD:AuthorizationUrl"]),
+                TokenUrl = new Uri(builder.Configuration["SwaggerAzureAD:TokenUrl"]),
+                Scopes = new Dictionary<string, string>
+                {
+                    { builder.Configuration["SwaggerAzureAD:Scope"], "Access API as user" },
+                }
+            }
+        },
+        In = ParameterLocation.Header,
+        Description = "OAuth2 Authorization Code Grant"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "OAuth2" }
+            },
+            new[] { builder.Configuration["SwaggerAzureAD:Scope"] }
+        }
+    });
+});
 
 builder.Services.AddScoped<AppDbContext>();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
@@ -25,11 +61,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.OAuthClientId(builder.Configuration["SwaggerAzureAD:ClientId"]);
+        options.OAuthUsePkce();
+        options.OAuthScopeSeparator(" ");
+    });
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAllOrigins");
 app.UseAuthentication();
 
 app.UseAuthorization();
